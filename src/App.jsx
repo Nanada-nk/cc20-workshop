9c312892d6270176e83e6bc945d1c99e663d3a96
Nanada-nk <nada.nidchakul@gmail.com>
2025-05-16 09:21:18 +0700

    starter with daisyui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-3xl text-amber-600">App</h1>
+      <button className="btn btn-primary">OK</button>
+    </div>
+  );
+}
+
+export default App;
